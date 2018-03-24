5ae2ca2d7a48b028381a80cdd346782d9b1bedee
Saurav <sauravmishra554@gmail.com>
2018-03-24 12:56:12 +0530

    sign in and student sign up skin

---
+++ Canteenos/app/src/main/java/com/example/android/canteenos/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.canteenos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button signIn = (Button) findViewById(R.id.signIn);
+
+        // Set a click listener on that View
+        signIn.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the Sign in category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                Intent signInIntent = new Intent(MainActivity.this, SignInActivity.class);
+
+                // Start the new activity
+                startActivity(signInIntent);
+            }
+
+        });
+
+        Button studentSignUp = (Button) findViewById(R.id.studentSignUp);
+
+        // Set a click listener on that View
+        studentSignUp.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the Sign in category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                Intent studentSignUpIntent = new Intent(MainActivity.this, StudentSignUpActivity.class);
+
+                // Start the new activity
+                startActivity(studentSignUpIntent);
+            }
+
+        });
+    }
+}
---
+++ Canteenos/app/src/main/java/com/example/android/canteenos/StudentSignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.canteenos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by saurav on 24/3/18.
+ */
+
+public class StudentSignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_sign_up);
+    }
+}
